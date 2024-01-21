609dfd44eb5993b93f216a3bd3d97926a137e6dd
Jait Jacob <jai8.jacob@gmail.com>
2024-01-22 00:42:16 +0530

    install & configure tailwind

--- code/maître_d/src/App.jsx
+++ code/maître_d/src/App.jsx
@@ -1,10 +1,32 @@
 import "./App.css";
+import logo from "./assets/logo.png";
+
+/*
+Header
+  Navbar
+Body
+ Search 
+ Restaurant Container
+  Restaurant Cards
+Footer
+  Copyright
+*/
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img height={250} width={250} src={logo} />
+      </div>
+      <p>Maitre D</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <>
-      <h1>lets gooooo</h1>
-    </>
+    <div className="app">
+      <Header></Header>
+    </div>
   );
 }
 
