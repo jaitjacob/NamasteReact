2d5a410d7ec67209e03f30a2dbe912a745e18c90
Jait Jacob <jai8.jacob@gmail.com>
2024-01-31 22:43:26 +0530

    add button with filter icon and click handler

--- code/maître_d/src/components/App.jsx
+++ code/maître_d/src/components/App.jsx
@@ -3,13 +3,14 @@ import Header from "./Header";
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
 import { useState } from "react";
+import FilterButton from "./FilterButton";
 
 function App() {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
     <div className="app">
       <Header />
-
+      <FilterButton></FilterButton>
       <div className="flex flex-wrap justify-start border border-red-600">
         {listOfRestaurants.map((restaurant) => (
           <RestaurantCard resData={restaurant.info} />
